f14f8ac3d72275f44274aae5a84cee4d2bbd9b45
Stephanie Peñaloza Cortés <spenalozacortes@gmail.com>
2023-08-18 11:41:22 -0600

    Initial commit

---
+++ src/test/java/com/example/boots/BootsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
